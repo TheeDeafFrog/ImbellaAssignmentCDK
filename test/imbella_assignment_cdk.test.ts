673fe9cd9e71a12219db039d6e2656b03b6b6b83
Kevin <36723751+TheeDeafFrog@users.noreply.github.com>
2023-03-24 14:02:11 +0200

    Initial commit

---
+++ test/imbella_assignment_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ImbellaAssignmentCdk from '../lib/imbella_assignment_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/imbella_assignment_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ImbellaAssignmentCdk.ImbellaAssignmentCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
